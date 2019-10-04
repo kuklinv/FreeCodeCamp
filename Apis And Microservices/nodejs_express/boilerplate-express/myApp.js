d9204303ab5f8a8ed72af9ec68a9ba1ea778c709
Vitaly <vitalykuklin@gmail.com>
2019-10-04 17:29:54 +0700

    Feature: add middleware function to node exe

--- Apis And Microservices/nodejs_express/boilerplate-express/myApp.js
+++ Apis And Microservices/nodejs_express/boilerplate-express/myApp.js
@@ -13,23 +13,42 @@ console.log('Hello World');
 
 
 /** 2) A first working Express Server */
-
+app.get('/', (req, res) => {
+ res.send('Hello express!');
+});
 
 /** 3) Serve an HTML file */
-
+app.get('/', (req, res) => {
+ res.sendFile(__dirname + '/views/index.html');
+});
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+app.get('/json', (req, res) => {
+ res.json({"message": "Hello JSON"});
+});
 
 /** 6) Use the .env file to configure the app */
- 
+app.get("/json", function(req, res) {
+  if (process.env.MESSAGE_STYLE ==  'uppercase') {
+    res.json({"message": "HELLO JSON"});
+  } else {
+      res.json({"message": "Hello json"});
+  }
+});
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
-
+app.get(
+    "/",
+    app.use(function middleware(req, res, next) {
+     var string = req.method + " " + req.path + " - " + req.ip;
+     console.log(string);
+     next();
+    })
+);
 
 /** 8) Chaining middleware. A Time server */
 
